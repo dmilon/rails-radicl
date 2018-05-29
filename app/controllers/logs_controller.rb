b9c8c34c1dd830cb343971c8e1751d541bc8ba84
Louisa Malki-Haegel <malkilouisa@gmail.com>
2018-05-29 17:52:41 +0200

    Workin on logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,22 @@
 class LogsController < ApplicationController
+
+  def index
+    @logs = Log.all
+  end
+
+  def new
+    @log = Log.new
+    @log_scope = LogScope.new
+  end
+
+  def create
+    @log = Log.new(params_log)
+
+  end
+
+  private
+
+  def params_log
+    params.require(:log).permit(:category, :date, :status, :description, :quantity)
+  end
 end
