2ee9d966a6d9b2541aa118bae426844bfd983ef1
Louisa Malki-Haegel <malkilouisa@gmail.com>
2018-05-29 11:50:44 +0200

    Added name to elements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_093901) do
+ActiveRecord::Schema.define(version: 2018_05_29_094836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_093901) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity"
+    t.string "name"
     t.index ["zone_id"], name: "index_elements_on_zone_id"
   end
 
