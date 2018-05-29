122cb96ace123d5162d6459371220c17e3060cf9
bruno della mattia <brunodellamattia@gmail.com>
2018-05-29 13:07:56 +0200

    first elements

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,4 +2,5 @@ class Garden < ApplicationRecord
   has_many :users
   has_many :follows
   has_many :zones
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   belongs_to :garden
   has_many :logs
   has_many :follows
+
+
 end
