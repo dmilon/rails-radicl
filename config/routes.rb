9c138f7fa418bc7008a13b7ce9876422821c1d33
Alexia Berro <alexia.berro@gmail.com>
2018-05-29 11:56:32 +0200

    zones create / link to do with element form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gardens, only: [:show] do
+    resources :zones, only: [:new, :create]
+  end
 end
