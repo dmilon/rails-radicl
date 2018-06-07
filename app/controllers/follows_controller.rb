3d40942bac98fc6cfe1c860d01693fbab188a064
Damien Milon <milon.damien@gmail.com>
2018-06-07 12:35:09 +0200

    implemented ajax for follow button on garden page and permacole friends on community page plus fixed bug for community farm list

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -10,7 +10,7 @@ class FollowsController < ApplicationController
   end
 
   def create
-    @gardens = Garden.all
+    @gardens = Garden.all.order(name: :desc)
     @garden = Garden.find(params[:garden_id])
     @follow = Follow.find_by(user: current_user, garden: @garden)
     if @follow
@@ -26,6 +26,27 @@ class FollowsController < ApplicationController
       @follow.save
       respond_to do |format|
         format.html { redirect_to community_gardens_path }
+        format.js
+      end
+    end
+  end
+
+  def update
+    @garden = Garden.find(params[:id])
+    @follow = Follow.find_by(user: current_user, garden: @garden)
+    if @follow
+      authorize @follow
+      @follow.destroy
+      respond_to do |format|
+        format.html { redirect_to garden_path(@garden) }
+        format.js
+      end
+    else
+      @follow = Follow.new(user: current_user, garden: @garden)
+      authorize @follow
+      @follow.save
+      respond_to do |format|
+        format.html { redirect_to garden_path(@garden) }
         format.js
       end
     end
