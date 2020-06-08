d8d02c4d5550735390c9e2a499bc0ffb37ae4667
Damien Milon <milon.damien@gmail.com>
2020-06-08 23:01:03 +0200

    clean git statu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @gardens = Garden.all
+    # @gardens = Garden.all
+    @gardens = []
   end
 
 end
